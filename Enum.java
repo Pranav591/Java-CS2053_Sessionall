0df33e06b2f181334972d4fe23094834aa079c4d
PRANAV ADHIKARI <pranavadhikaribsc24@rvu.edu.in>
2025-10-16 04:39:39 +0700

    file added successfully

---
+++ Enum.java
@@ -0,0 +1,19 @@
+
+enum Level {
+    LOW, MEDIUM, HIGH;
+
+    void display() {
+        System.out.println("Level: " + this);
+    }
+}
+
+public class Enum {
+    public static void main(String[] args) {
+        Level l = Level.HIGH;
+        l.display();
+
+        for (Level x : Level.values()) {
+            System.out.println(x + " at index " + x.ordinal());
+        }
+    }
+}
